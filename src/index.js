10ca950af9490a670613bf04a7ab92ba0c149f72
Yuriy <ubratica@gmail.com>
2023-06-22 20:18:40 +0300

    add component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-const el = <h1>Hello World</h1>
+
+const Title = () => {
+    return <h1>Hello World</h1>
+}
+
+const List = () => {
+    return (
+        <ul>
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li>List item 3</li>
+        </ul>
+    )
+}
+
+const el = (
+    <div>
+        <Title />
+        <List />
+    </div>
+)
 
 ReactDOM.render(el, document.getElementById('root'))
