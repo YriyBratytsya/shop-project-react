f3c768164ec91abaf9db2d14ad5b03a24de5f717
Yuriy <ubratica@gmail.com>
2023-06-26 16:30:30 +0300

    added exsport

---
+++ src/container/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const App = () => {
+    return (
+        <>
+            <h1>Hello App.js</h1>
+        </>
+    )
+}
+
+// export default App
--- src/index.js
+++ src/index.js
@@ -1,38 +1,5 @@
-import PropTypes from 'prop-types'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { App } from './container/App.js'
 
-const Title = ({ text, id }) => {
-    return (
-        <h1>
-            Hello {text}, {id}
-        </h1>
-    )
-}
-Title.propTypes = {
-    text: PropTypes.string,
-    id: PropTypes.number.isRequired,
-}
-Title.defaultProps = {
-    text: 'world',
-}
-
-const List = () => {
-    return (
-        <ul>
-            <li>List item 1</li>
-            <li>List item 2</li>
-            <li>List item 3</li>
-        </ul>
-    )
-}
-
-const el = (
-    <>
-        <Title text="Jack" id={1} />
-        <Title id={2} />
-        <List />
-    </>
-)
-
-ReactDOM.render(el, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
